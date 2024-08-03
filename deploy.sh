308f80515f1b758d2d1ffdf5c6a86ba58c75e769
Abishan Sutharshan <abishan.sutharshan@gmail.com>
2024-08-02 21:51:35 -0700

    set up quarto site

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,8 @@ git add .
 git commit -m "Build site"
 git push origin main
 
+# Ensure the latest changes from gh-pages are pulled
+git pull origin gh-pages
+
 # Deploy the _site directory to gh-pages branch
 git subtree push --prefix my-quarto-project/_site origin gh-pages --force
